c5a6ccdf2deb827cf7ff82963eb690fc7fb48e3f
ExcellentGamer <75348481+ExcellentGamer@users.noreply.github.com>
2022-10-03 21:01:03 -0400

    wait nvm

--- Development/ExcellentGamer/actors/file_a/geo_header.h
+++
@@ -1,9 +0,0 @@
-extern const GeoLayout file_a_geo[];
-extern Lights1 file_a_f3d_material_lights;
-extern u8 file_a_main_menu_seg7_us_0B841_rgba32[];
-extern Vtx file_a_000_displaylist_mesh_layer_4_vtx_0[4];
-extern Gfx file_a_000_displaylist_mesh_layer_4_tri_0[];
-extern Gfx mat_file_a_f3d_material[];
-extern Gfx mat_revert_file_a_f3d_material[];
-extern Gfx file_a_000_displaylist_mesh_layer_4[];
-extern Gfx file_a_material_revert_render_settings[];
--- Development/ExcellentGamer/actors/file_c/geo_header.h
+++
@@ -1,9 +0,0 @@
-extern const GeoLayout file_c_geo[];
-extern Lights1 file_c_f3d_material_lights;
-extern u8 file_c_main_menu_seg7_us_0B843_rgba16[];
-extern Vtx file_c_000_displaylist_mesh_layer_4_vtx_0[4];
-extern Gfx file_c_000_displaylist_mesh_layer_4_tri_0[];
-extern Gfx mat_file_c_f3d_material[];
-extern Gfx mat_revert_file_c_f3d_material[];
-extern Gfx file_c_000_displaylist_mesh_layer_4[];
-extern Gfx file_c_material_revert_render_settings[];
